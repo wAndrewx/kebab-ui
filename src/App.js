044f482dbb480ddedaf65a59881ef80c979150ac
Andrew Huynh <andrew.huynh17@gmail.com>
2021-08-27 22:03:13 -0400

    init route implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { useCallback, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ChakraProvider, Box, theme } from '@chakra-ui/react';
 import { TwitterPage } from '../src/pages/TwitterPage';
+import { LoginPage } from '../src/pages/LoginPage';
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="start" fontSize="xl">
-        <TwitterPage />
-      </Box>
+      <Router>
+        <Route path="/feed">
+          <Box textAlign="start" fontSize="xl">
+            <TwitterPage />
+          </Box>
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+      </Router>
     </ChakraProvider>
   );
 }
