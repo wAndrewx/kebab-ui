9dd6470ffae2fcad662405bdb783937080e7ad71
Andrew Huynh <andrew.huynh17@gmail.com>
2021-08-28 21:02:34 -0400

    tweet component and feed looks lots better

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,9 +1,9 @@
-import { Box, Textarea } from '@chakra-ui/react';
+import { Box, Button, Flex, Textarea, VStack } from '@chakra-ui/react';
 import { Tweet } from './Tweet';
 //tweets
 export const Feed = () => {
   return (
-    <Box height="100vh" borderX="1px" borderColor="gray.500" maxW="600px">
+    <Box height="100%" borderX="1px" borderColor="gray.500" maxW="600px">
       {/* get tweet in here and then put shit in <Tweet> */}
       <Box
         pt="2"
@@ -13,17 +13,53 @@ export const Feed = () => {
         display="flex"
         px="4"
         fontWeight="bold"
-        position="static"
+        position="relative"
         borderBottom="1px"
         borderColor="gray.500"
-        display="flex"
+        zIndex="2"
       >
         Latest Tweets
       </Box>
-      <Box>
-        <Textarea resize="none" />
+      <Box
+        borderBottom="1px"
+        borderColor="gray.500"
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Flex
+          boxSize="14"
+          bg="blue.200"
+          rounded="full"
+          align="center"
+          justifyContent="center"
+          fontWeight="bold"
+          mx="2"
+          mt="4"
+        >
+          A
+        </Flex>
+        <VStack align='end' w='100%'> 
+          <Textarea
+            resize="none"
+            border="0"
+            border="1px"
+            alignSelf="baseline"
+          />
+          <Button>Tweet</Button>
+        </VStack>
       </Box>
-      <Box position="absolute" maxW="600px" w='100%' >
+      <Box maxW="600px" w="600px">
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
+        <Tweet />
         <Tweet />
       </Box>
     </Box>
--- src/components/feed/Tweet.js
+++ src/components/feed/Tweet.js
@@ -2,23 +2,53 @@ import { Box, Button, Flex, IconButton, Stack, Text } from '@chakra-ui/react';
 import { ChatIcon, Icon, RepeatIcon, StarIcon } from '@chakra-ui/icons';
 import { TweetButtons } from './TweetButtons';
 
-export const Tweet = () => {
+export const Tweet = ({ name }) => {
+  name = 'Andrew';
   return (
-    <Box bg="red.100" h="205px">
-      <Flex></Flex>
-      <Stack direction="column">
-        <Box id="username" bg="salmon">
-          @andrew
+    <Box display="flex" borderBottom="1px" borderColor="gray.500">
+      <Flex
+        boxSize="14"
+        bg="blue.200"
+        rounded="full"
+        align="center"
+        justifyContent="center"
+        fontWeight="bold"
+        mx="2"
+        mt="4"
+        position="absolute"
+      >
+        {name[0]}
+      </Flex>
+      <Stack direction="column" mt="4" ml="20">
+        <Box
+          id="username"
+          display="flex"
+          alignSelf="baseline"
+          fontWeight="bold"
+          px="2"
+        >
+          andrew
         </Box>
-        <Box id="content" bg="blue.300">
-          tweet here
+        <Box
+          id="content"
+          fontSize="md"
+          display="flex"
+          alignSelf="baseline"
+          m="0"
+          p="2"
+        >
+          tweet herewefaefa tweet hereefaefweet here tweet hereawefawef tweet
+          here tweet herasfefae tweet heasefaefre tweet here tweet here tweet
+          here tweet here tweet here tweet here twaefawt here tweet here tweet
+          here tweet here tweet here
         </Box>
         <Box
           id="tweet-buttons"
-          bg="green.200"
           display="flex"
           alignItems="baseline"
-          justifyContent="space-evenly"
+          justifyContent="space-between"
+          pr="16"
+          m="0"
         >
           <TweetButtons type={<ChatIcon />} number="2" />
           <TweetButtons type={<RepeatIcon />} number="2" />
