6a9bb142cdf12cdf0e0f2f4ebbd6a8a295a0889a
Andrew Huynh <andrew.huynh17@gmail.com>
2021-08-30 19:58:15 -0400

    fix nav positioning

--- src/pages/TwitterPage.js
+++ src/pages/TwitterPage.js
@@ -1,14 +1,14 @@
 import { Navigation } from '../components/Navigation';
 import { Feed } from '../components/feed/Feed';
-import { Flex, Grid, GridItem, Stack } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 export const TwitterPage = () => {
   return (
     <>
       <Grid templateColumns="repeat(3, 1fr)">
-        <GridItem colStart="1" justifySelf="end">
+        <GridItem colStart="1" position="fixed" left="72">
           <Navigation />
         </GridItem>
-        <GridItem colStart="2" >
+        <GridItem colStart="2">
           <Feed />
         </GridItem>
         <GridItem colStart="3"></GridItem>
