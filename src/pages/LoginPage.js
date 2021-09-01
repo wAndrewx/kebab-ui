790789c57bcf8846bb141499053e806fea8e0471
Andrew Huynh <andrew.huynh17@gmail.com>
2021-08-31 23:07:05 -0400

    layout of login and function finished, just need to redirect to feed

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,10 @@
 import {
   Box,
+  Divider,
   Flex,
+  Image,
+  Link,
+  Portal,
   Tab,
   TabList,
   TabPanel,
@@ -10,15 +14,54 @@ import {
 } from '@chakra-ui/react';
 import { Register } from '../components/auth/Register';
 import { Login } from '../components/auth/Login';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 export const LoginPage = () => {
   return (
     <Flex direction="row" h="100%">
-      <Box bg="red.900" id="landing-page-info" w="70%" h="100vh">
-        <Text id="landing-title">Twitter mock app interface</Text>
-        <Text>
-          This is the front end of the twitter mock app. The API is limited to
-          tweets and authentication only
+      <Box
+        id="landing-page-info"
+        w="70%"
+        h="100vh"
+        flexWrap="wrap"
+        backgroundImage="login-2.jpg"
+        backgroundSize="cover"
+        backgroundPosition="center"
+      >
+        <Text
+          id="landing-title"
+          fontWeight="bold"
+          fontSize="8xl"
+          mt="20"
+          lineHeight="short"
+          px="16"
+          w="60vw"
+          color="blackAlpha.900"
+        >
+          Twitter Mock Interface
+        </Text>
+        <Text
+          id="landing-body"
+          mt="20"
+          fontSize="xl"
+          p="8"
+          w="60vw"
+          color="whiteAlpha.900"
+          bg="blackAlpha.600"
+          rounded="2xl"
+          m="16"
+        >
+          This is the front end, to interface the Twitter mock API. The API is
+          hosted on AWS Lambda using serverless and managed by AWS API gateway.
+          <Divider py="2" />
+          You can checkout the code{' '}
+          <Link
+            textDecoration="underline"
+            href="https://github.com/wAndrewx/kebab-api"
+          >
+            here
+            <ExternalLinkIcon mx="2px" />
+          </Link>
         </Text>
       </Box>
 
